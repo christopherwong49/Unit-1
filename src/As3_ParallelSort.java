6a49220bbaf82d85f57f2b1498c1992cb8e1ab45
Christopher Wong <cwong8864@gmail.com>
2024-10-18 11:20:14 -0600

    10/18/2024

--- src/As3_ParallelSort.java
+++ src/As3_ParallelSort.java
@@ -1,53 +1,69 @@
 public class As3_ParallelSort {
-    public static String airport[] = MyFiles.loadStringArr("Unit-1/IntelliJ_DataSet/BusiestAirports/airport.txt");
-    public static String country[] = MyFiles.loadStringArr("Unit-1/IntelliJ_DataSet/BusiestAirports/country.txt");
-    public static int totalpassengers[] = MyFiles.loadIntArr("Unit-1/IntelliJ_DataSet/BusiestAirports/totalpassengers.txt");
-    public static int year[] = MyFiles.loadIntArr("Unit-1/IntelliJ_DataSet/BusiestAirports/year.txt");
+    public static String airport[] = MyFiles.loadStringArr("IntelliJ_DataSet/BusiestAirports/airport.txt");
+    public static String country[] = MyFiles.loadStringArr("IntelliJ_DataSet/BusiestAirports/country.txt");
+    public static int totalpassengers[] = MyFiles.loadIntArr("IntelliJ_DataSet/BusiestAirports/totalpassengers.txt");
+    public static int year[] = MyFiles.loadIntArr("IntelliJ_DataSet/BusiestAirports/year.txt");
 
     public static void run() {
-        while(true) {
+        while (true) {
             System.out.println("Press 1 to sort by airport\nPress 2 to print/sort by country\nPress 3 to filter by year\nPress 4 to total passengers by airport\nPress 5 to search for airport\nPress 6 to exit");
 
             int option = Library.input.nextInt();
 
-            if(option == 1) {
+            if (option == 1) {
                 sortStringArr(airport);
                 for (int i = 0; i < airport.length; i++) {
                     System.out.println(country[i] + " " + airport[i] + " " + totalpassengers[i] + " " + year[i]);
                 }
             }
 
-            if(option == 2) {
+            if (option == 2) {
                 sortStringArr(country);
                 for (int i = 0; i < airport.length; i++) {
                     System.out.println(country[i] + " " + airport[i] + " " + totalpassengers[i] + " " + year[i]);
                 }
             }
 
-            if(option == 3) {
+            if (option == 3) {
                 System.out.println("What year do you want to filter by?");
                 int answer = Library.input.nextInt();
                 for (int i = 0; i < airport.length; i++) {
-                    if(answer == year[i]) {
+                    if (answer == year[i]) {
                         System.out.println(country[i] + " " + airport[i] + " " + totalpassengers[i] + " " + year[i]);
                     }
                 }
             }
 
-            if(option == 4) {
+            if (option == 4) {
                 System.out.println("For what airport do you want total passengers?");
                 Library.input.nextLine();
                 String airportSelected = Library.input.nextLine();
                 int total = 0;
                 for (int i = 0; i < airport.length; i++) {
-                    if(airportSelected.equals(airport[i])) {
+                    if (airportSelected.equals(airport[i])) {
                         total += totalpassengers[i];
                     }
                 }
                 System.out.println("Total passengers: " + total);
             }
 
-            if(option == 6) {
+            if (option == 5) {
+                System.out.println("What airport do you want to search for?");
+                Library.input.nextLine();
+                String answer1 = Library.input.nextLine();
+                boolean found = false;
+                for (int i = 0; i < airport.length; i++) {
+                    if (answer1.equals(airport[i])) {
+                        System.out.println(country[i] + " " + airport[i] + " " + totalpassengers[i] + " " + year[i]);
+                        found = true;
+                    }
+                }
+                if (found == false) {
+                    System.out.println("Airport not found");
+                }
+            }
+
+            if (option == 6) {
                 break;
             }
 
